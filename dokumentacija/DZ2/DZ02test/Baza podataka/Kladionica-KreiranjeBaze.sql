100bfb29bcf0069fcf547d81fcd463a0d447b1c4
jivancevic <josip.ivancevic@fer.hr>
2022-05-13 17:57:09 +0200

    Nez sto san ode radi

--- dokumentacija/DZ2/DZ02test/Baza podataka/Kladionica-KreiranjeBaze.sql
+++ dokumentacija/DZ2/DZ02test/Baza podataka/Kladionica-KreiranjeBaze.sql
@@ -1,28 +1,22 @@
-CREATE database kladionica;
-
 CREATE table korisnik (
 	id VARCHAR(50) PRIMARY KEY,
 	korisnicko_ime VARCHAR(30) NOT NULL UNIQUE,
 	saldo NUMERIC NOT NULL
 );
-
 CREATE table uplata (
 	id VARCHAR(50) PRIMARY KEY,
 	korisnik_id VARCHAR(50) NOT NULL,
 	iznos NUMERIC NOT NULL,
 	CONSTRAINT fk_korisnik FOREIGN KEY (korisnik_id) REFERENCES korisnik(id)
 );
-
 CREATE table sport (
 	id VARCHAR(50) PRIMARY KEY,
 	ime VARCHAR(30) NOT NULL
 );
-
 CREATE table natjecatelj (
 	id VARCHAR(50) PRIMARY KEY,
 	ime VARCHAR(30) NOT NULL
 );
-
 CREATE table dogadaj (
 	id VARCHAR(50) PRIMARY KEY,
 	naziv VARCHAR(100) NOT NULL,
@@ -34,7 +28,6 @@ CREATE table dogadaj (
 	CONSTRAINT fk_natjecatelj_domacin FOREIGN KEY (domacin_id) REFERENCES natjecatelj(id),
 	CONSTRAINT fk_natjecatelj_gost FOREIGN KEY (gost_id) REFERENCES natjecatelj(id)
 );
-
 CREATE table ponuda (
 	id VARCHAR(50) PRIMARY KEY,
 	dogadaj_id VARCHAR(50) NOT NULL,
@@ -43,7 +36,6 @@ CREATE table ponuda (
 	završeno BOOLEAN DEFAULT FALSE NOT NULL,
 	CONSTRAINT fk_dogadaj FOREIGN KEY (dogadaj_id) REFERENCES dogadaj(id)
 );
-
 CREATE table ishod (
 	id VARCHAR(50) PRIMARY KEY,
 	ponuda_id VARCHAR(50) NOT NULL,
@@ -52,14 +44,12 @@ CREATE table ishod (
 	dobitan BOOLEAN DEFAULT NULL,
 	CONSTRAINT fk_ponuda FOREIGN KEY (ponuda_id) REFERENCES ponuda(id)
 );
-
 CREATE table listic (
 	id VARCHAR(50) PRIMARY KEY,
 	korisnik_id VARCHAR(50) NOT NULL,
 	uplata NUMERIC NOT NULL,
 	CONSTRAINT fk_korisnik FOREIGN KEY (korisnik_id) REFERENCES korisnik(id)
 );
-
 CREATE table listic_ishod (
 	listic_id VARCHAR(50) NOT NULL,
 	ishod_id VARCHAR(50) NOT NULL,
